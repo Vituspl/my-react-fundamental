137257ad7e68cc110ffeae3efdd95fba1f3e3a43
VitusPl <victorplug@gmail.com>
2023-11-24 18:13:44 +0800

    Подключаем стили App.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Counter from './components/Counter';
 import ClassCounter from './components/ClassCounter';
+import './styles/App.css';
 
 // Корневой компонент App импортирует компонент Counter,
 // и передает его в дом дерево ReactDOM
@@ -8,8 +9,17 @@ function App() {
 
     return (
         <div className="App">
-            <Counter/>
-            <ClassCounter/>
+            <div className="post">
+                <div className="post__content">
+                    <strong>1. JavaScript</strong>
+                    <div>
+                        JavaScript - язык программирования
+                    </div>
+                </div>
+                <div className="post__btns">
+                    <button>Удалить</button>
+                </div>
+            </div>
         </div>
     );
 }
